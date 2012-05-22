65c552126072b2471c24c0e70b250f197b7093d5
Anton Sidelnikov <ndmeredian@gmail.com>
2012-05-22 14:09:43 +0700

    Initial commit

---
+++ lib/simplecov-jscoverage.rb
@@ -0,0 +1,7 @@
+require "simplecov-jscoverage/version"
+
+module Simplecov
+  module Jscoverage
+    # Your code goes here...
+  end
+end
---
+++ lib/simplecov-jscoverage/version.rb
@@ -0,0 +1,5 @@
+module Simplecov
+  module Jscoverage
+    VERSION = "0.0.1"
+  end
+end
